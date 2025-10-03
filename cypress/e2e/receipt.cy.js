6c8832bec1afe6dd78820554d966ea94fd714572
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-10-03 11:29:34 -0400

    more testing

--- cypress/e2e/receipt.cy.js
+++ cypress/e2e/receipt.cy.js
@@ -5,12 +5,8 @@
 describe('Receipt', () => {
   it('shows the price on the receipt', () => {
     cy.visit('app/index.html')
-    cy.get('#grand-total')
-      .invoke('text')
-      .should('be.a', 'string')
-      .then((totalText) => {
-        cy.contains('button', 'View Summary').click()
-        cy.get('#dialog-total-value').should('have.text', totalText)
-      })
+    cy.get('#grand-total').should('have.text', '$304.97')
+    cy.contains('button', 'View Summary').click()
+    cy.get('#dialog-total-value').should('have.text', '304.97')
   })
 })
